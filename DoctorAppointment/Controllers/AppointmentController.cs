678fb103ca18fb7d8e226496f7af0637ac730b36
Pritam Jay <pjay@gmail.com>
2025-09-16 17:48:52 +0530

    commit

--- DoctorAppointment/Controllers/AppointmentController.cs
+++ DoctorAppointment/Controllers/AppointmentController.cs
@@ -40,7 +40,7 @@ namespace DoctorAppointment.Controllers
                     aId = a.AId,
                     pname = a.Pname,
                     email = a.Email,
-                    doctorMaster = new { name = a.DoctorMaster.Name }, // Ensure this field exists
+                    doctorMaster = new { name = a.DoctorMaster.Name }, 
                     date = a.Date,
                     fromtime = a.Fromtime,
                     totime = a.Totime,
