9d1683c684da722745d91383e9e10dbf0b10514e
Pritam Jay <pjay@gmail.com>
2025-09-17 17:26:48 +0530

    check for test

--- DoctorAppointment/Controllers/HomeController.cs
+++ DoctorAppointment/Controllers/HomeController.cs
@@ -186,10 +186,6 @@ namespace DoctorAppointment.Controllers
         }
 
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult unuse()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+       
     }
 }
